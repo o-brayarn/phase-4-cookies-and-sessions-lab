0cc27bcf6bb279e818803af0251326b8939f158b
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-12-02 07:24:11 +0300

    completed cookies-and-sessions-lab

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,13 @@ class ArticlesController < ApplicationController
 
   def show
     article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    if session[:page_views] <= 3
+      render json: article
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
   end
 
   private
@@ -16,5 +22,4 @@ class ArticlesController < ApplicationController
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
-
 end
